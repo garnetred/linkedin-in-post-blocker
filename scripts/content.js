7bba8b39217cb2965e791d14232c52bf23de1cf3
December G <dgarnetsmith@gmail.com>
2024-05-29 11:10:44 -0500

    Create basic MVP to hide the word AI in posts

--- scripts/content.js
+++ scripts/content.js
@@ -2,27 +2,22 @@ let posts = document.querySelectorAll('.update-components-text')
 let main = document.querySelector('.scaffold-finite-scroll__content')
 let config = {childList: true, characterData: true, subtree: true}
 
-const checkForRestrictedWords = () => {
-    console.log('checking for restricted words')
-    for (let i = 0; i < posts.length; i++) {
-        console.log(i)
-        console.log(posts[i].innerText)
-        if (post[i].innerText.includes('ancestors') || posts[i].innerText.includes('AI')) {
-            console.log('post including the word ancestors or AI found')
-            i.classList.add('hide')
-        }
+const checkForRestrictedWords = (text, post) => {
+    if (text.innerText.split(' ').includes("AI")) {
+        console.log('post includes restricted word')
+        console.log(text.innerText)
+        post.classList.add('hide')
     }
 }
 
 const callback = (mutationList, observer) => {
-    // console.log(main)
-    if (mutationList.mutation)
-    if (posts.length) {
-        console.log(posts, 'posts')
-        checkForRestrictedWords();
-    } else {
-        observer.disconnect()
+    mutationList.forEach(mutation => {
+    if (mutation.target.classList.contains('feed-shared-update-v2')) {
+        let container = mutation.target
+        let update = container.querySelector('.update-components-text')
+        checkForRestrictedWords(update, container);
     }
+})
 }
 
 const observer = new MutationObserver(callback)
