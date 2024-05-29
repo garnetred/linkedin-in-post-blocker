d46156588a4a4a19ff3bcf3e0a8f7a75d1949a81
December G <dgarnetsmith@gmail.com>
2024-05-29 10:23:38 -0500

    Attempt to hide specific posts

--- scripts/content.js
+++ scripts/content.js
@@ -1,12 +1,38 @@
-let posts = document.querySelector('.update-components-text')
-let config = {attributes: true, childList: true, subtree: true}
+let posts = document.querySelectorAll('.update-components-text')
+let main = document.querySelector('.scaffold-finite-scroll__content')
+let config = {childList: true, characterData: true, subtree: true}
+
+const checkForRestrictedWords = () => {
+    console.log('checking for restricted words')
+    for (let i = 0; i < posts.length; i++) {
+        console.log(i)
+        console.log(posts[i].innerText)
+        if (post[i].innerText.includes('ancestors') || posts[i].innerText.includes('AI')) {
+            console.log('post including the word ancestors or AI found')
+            i.classList.add('hide')
+        }
+    }
+}
+
 const callback = (mutationList, observer) => {
-    if (document.contains(posts)) {
-        console.log(posts)
+    // console.log(main)
+    if (mutationList.mutation)
+    if (posts.length) {
+        console.log(posts, 'posts')
+        checkForRestrictedWords();
+    } else {
         observer.disconnect()
     }
 }
 
 const observer = new MutationObserver(callback)
 
-observer.observe(posts)
+observer.observe(main, config)
+
+
+// allEl.forEach(el => {
+//     if (el.innerText.includes('ancestors')) {
+//         console.log('text found');
+//         console.log(el.innerText)
+//     }
+// })
