e4107c8a9c1f5ec7a7651e0b84cbba0d6f3df030
December G <dgarnetsmith@gmail.com>
2024-05-30 09:08:01 -0500

    Add comment about local storage

--- popup.js
+++ popup.js
@@ -1,13 +1,16 @@
 const form = document.querySelector("form");
+const textarea = document.querySelector("textarea");
+let value = null;
 console.log(form);
 
 function updateRestrictedWords(event) {
   event.preventDefault();
-  console.log(event);
-  console.log(event.target.textarea);
-  const data = new formData(event.target);
-  console.log(data.get("textarea"));
-  //   console.log(event.target.textarea.value);
+  console.log(value);
+  // add value to local storage in an array
 }
 
 form.addEventListener("submit", (event) => updateRestrictedWords(event));
+
+textarea.addEventListener("change", (event) => {
+  value = event.target.value;
+});
